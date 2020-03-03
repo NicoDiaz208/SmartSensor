cc7f0207872822ed925ece9591b27385bbe85acf
NicoDiaz208 <nicodiaz208@gmail.com>
2020-03-03 12:58:05 +0100

    Angular Client für Belmin hergerichtet

--- AngularClient/AngularClient/src/app/app.component.ts
+++ AngularClient/AngularClient/src/app/app.component.ts
@@ -1,34 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import * as mqttt from 'mqtt'
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-
-
-
-export class AppComponent implements OnInit {
-  ngOnInit(): void {
-    this.client.subscribe('topic')
-    this.client.on('message', (topic,message)=>{
-      console.log(message.toString())
-      this.client.publish('topic','Hello mqtt')
-      this.client.end;
-    })
-
-    
-  }
+export class AppComponent {
   title = 'AngularClient';
-  client = mqttt.connect('http://localhost:8080',{
-    username : 'admin',
-    password : 'hivemq'
-  });
-
-  
-  
-  constructor(){
-    
-  }
 }
