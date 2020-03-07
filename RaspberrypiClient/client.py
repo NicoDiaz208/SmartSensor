c7d3c8f283d0f606e9455c224fb693ece5103382
Nico Diaz <nicodiaz208@gmail.com>
2020-03-07 19:32:35 +0000

    rbc modified

--- RaspberrypiClient/client.py
+++ RaspberrypiClient/client.py
@@ -12,17 +12,23 @@ def on_subscribe(client, userdata, mid, granted_qos):
     print("Subscribed: "+str(mid)+" "+str(granted_qos))
 
 def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.qos)+" "+str(msg.payload))  
+	if msg.payload == "on":
+		print("Light is ON")
+	print(msg.topic+" "+str(msg.qos)+" "+str(msg.payload))
 
-client = paho.Client()
+client = paho.Client(client_id="RaspberryPi_Nico")
+client.on_connect = on_connect
+client.on_publish = on_publish
 client.on_subscribe = on_subscribe
 client.on_message = on_message
-client.connect("169.254.219.0",8080)
-client.subscribe("encyclopedia/#",qos=1)
+client.connect("169.254.219.0",1883)
+client.subscribe("nico/raspberry",qos=1)
 client.loop_start()
 
 while True:
-	temperature = read_from_imaginary_thermometer()
-	(rc,mid) = client.publish("encyclopedia/temperature",str(temperature), qos=1)
-	time.sleep(30)
-
+	msg  = client.publish("SmartHome/Light","HelloWorld",qos=1)
+	time.sleep(10)
+	if msg.is_published() == False:
+		print("Message is not yet pushed")
+	else:
+		print("Message pushed")
