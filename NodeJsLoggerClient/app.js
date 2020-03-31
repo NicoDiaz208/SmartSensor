6b768ec6054077a0eb2f3a4be1a905bd029ec681
MarcoSupper <marco.supper2@gmail.com>
2020-03-31 13:08:29 +0200

    Reformating code

--- NodeJsLoggerClient/app.js
+++ NodeJsLoggerClient/app.js
@@ -1,17 +1,26 @@
-// MQTT
 const mqtt = require('mqtt');
 const options = {
     port: 1883,
     host: '172.17.217.90'
 }
 const client = mqtt.connect(options);
+const MongoClient = require('mongodb').MongoClient;
+const url = "mongodb://localhost";
+const dbName = "sensor"
+var db;
+var data;
+const express = require('express'),
+server = express(),
+port = 3000;
 
 
+
+
+
+// Subscribes to all Topics
 client.subscribe('#');
 
-// sends data.
-// client.publish('nico/raspberry','HelloWorld')
-
+// If the client receives data it will be inserted into the mongodb
 client.on('message', function(topic, message, packet) {
 	data.insert({
 		Timestamp: Date.now(),
@@ -25,36 +34,15 @@ client.on('message', function(topic, message, packet) {
 	});
 });
 
-client.on('connect', function(){
-    console.log('Connected');
-})
 
-
-
-
-
-
-
-
-// Mongo DB
-const MongoClient = require('mongodb').MongoClient;
-// For Docker Toolbox
-// const url = "mongodb://192.168.99.100:27017";
-
-// For Docker Desktop
-const url = "mongodb://localhost:27017";
-const dbName = "sensor"
-var db;
-var data;
-
-
+// Connects to the MongoClient
 MongoClient.connect(url, function(err, client) {
-  if (err) throw err;
-  console.log("Database Connection established!");
-
-  db = client.db(dbName);
-  
-  data = db.collection('data');
+	if (err) throw err;
+	console.log("Database Connection established!");
+	// Gets the Database by name (sensor)
+	db = client.db(dbName);
+	// Creates a connection to the collection named 'data'
+	data = db.collection('data');
 })
 
 
@@ -62,29 +50,26 @@ MongoClient.connect(url, function(err, client) {
 
 
 
-// Rest
-
-var express = require('express'),
-app = express(),
-port = 3000;
+// Sets the port the server will listen to (3000)
+server.listen(port);
+console.log('Server listening on port: '+port);
 
 
-app.get('/getAll', function (req, res) {
+// Gets all the data in the database
+server.get('/getAll', function (req, res) {
 	data.find({}).toArray((err, result) => {
 		if (err) throw err;
 		res.send(result);
 		console.log('Send all Data');
   }); 
 });
-app.get('/getTopic/:topic', function (req, res) {
+
+// Gets all the data in the database from the requested topic
+server.get('/getTopic/:topic', function (req, res) {
 	var query = { Topic: 'SmartHome/'+req.params.topic };
 	data.find(query).toArray((err, result) => {
-		if (err) throw err;
 		res.send(result);
+		if (err) throw err;
 		console.log('Send data from Topic: '+req.params.topic);
   }); 
-});
-
-app.listen(port);
-console.log('todo list RESTful API server started on: ' + port);
-  
+});  
