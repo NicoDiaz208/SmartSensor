27d98404230c1e12d4fc0fcd4c676594263cbedb
Litzlfellner Patrick <32123817+Litzlfellner@users.noreply.github.com>
2020-06-16 16:30:49 +0200

    Update Bme680_HiveMq.ino

--- Bme680_HiveMq/Bme680_HiveMq.ino
+++ Bme680_HiveMq/Bme680_HiveMq.ino
@@ -1,19 +1,4 @@
-/***************************************************************************
-  This is a library for the BME680 gas, humidity, temperature & pressure sensor
-
-  Designed specifically to work with the Adafruit BME680 Breakout
-  ----> http://www.adafruit.com/products/3660
-
-  These sensors use I2C or SPI to communicate, 2 or 4 pins are required
-  to interface.
-
-  Adafruit invests time and resources providing this open source code,
-  please support Adafruit and open-source hardware by purchasing products
-  from Adafruit!
-
-  Written by Limor Fried & Kevin Townsend for Adafruit Industries.
-  BSD license, all text above must be included in any redistribution
- ***************************************************************************/
+ 
 
 #include <Wire.h>
 #include <SPI.h>
